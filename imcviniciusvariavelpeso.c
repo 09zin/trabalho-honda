ce6bc46760d76f817dc365ff0cb05071f32d216f
Garfosata <vinimemosz@gmail.com>
2025-04-26 16:50:45 -0300

    Update imcviniciusvariavelpeso.c

--- imcviniciusvariavelpeso.c
+++ imcviniciusvariavelpeso.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
-int main()
-{
     float peso;
 
     printf("Qual o seu peso?");
     scanf ("%f", &peso);
     imc = peso/ (altura*altura);
     printf("seu imc é: %.2f\n",imc);
-}
-
